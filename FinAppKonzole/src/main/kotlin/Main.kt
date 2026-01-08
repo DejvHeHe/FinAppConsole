5a42265d21fca4290ad33e69d457dce6db69ea9a
David Sobíšek <dsobisek04@gmail.com>
2026-01-08 23:28:23 +0100

    Přidání výběru, sumarizace

--- FinAppKonzole/src/main/kotlin/Main.kt
+++ FinAppKonzole/src/main/kotlin/Main.kt
@@ -1,18 +1,57 @@
 import Models.Transaction
 import Models.TransactionCategory
 import Models.TransactionType
+var option:String="1"
+
 
 fun main()
 {
-    print("Zadejte název transakce:")
-    val transactionName=readln()
-    print("Zadejte typ transakce (INCOME/EXPENSE):")
-    val transactionType=readln().uppercase()
-    print("Zadejte kategori transakce:")
-    val transactionCategory=readln().uppercase()
-    print("Zadejte částku transakce:")
-    val transactionAmount=readln().toIntOrNull()?:0
 
-    val newRecord=Transaction(name=transactionName, amount=transactionAmount,   type= TransactionType.valueOf(transactionType), category = TransactionCategory.valueOf(transactionCategory))
-    TransactionManager.create(newRecord)
+    while (option!="0")
+    {
+
+        pickOption()
+
+    }
+
 }
+fun pickOption()
+{
+    println("0.Close")
+    println("1.Create transaction")
+    println("2.Remove transaction")
+    println("3.View all transaction")
+    println("4.View sumarization")
+    print("Enter option: ")
+    option=readln()
+
+    when(option)
+    {
+        "1"->{print("Zadejte název transakce:")
+            val transactionName=readln()
+            print("Zadejte typ transakce (INCOME/EXPENSE):")
+            val transactionType=readln().uppercase()
+            print("Zadejte kategori transakce:")
+            val transactionCategory=readln().uppercase()
+            print("Zadejte částku transakce:")
+            val transactionAmount=readln().toIntOrNull()?:0
+
+            val newRecord=Transaction(name=transactionName, amount=transactionAmount,   type= TransactionType.valueOf(transactionType), category = TransactionCategory.valueOf(transactionCategory))
+            TransactionManager.create(newRecord)
+        }
+        "2"->{
+
+        }
+        "3"->{
+           println(TransactionManager.getAll())
+        }
+        "4"->{
+            println("Vaš status je:${TransactionManager.sumarize()}")
+
+        }
+
+    }
+
+
+}
+
--- FinAppKonzole/src/main/kotlin/TransactionManager.kt
+++ FinAppKonzole/src/main/kotlin/TransactionManager.kt
@@ -1,4 +1,6 @@
 import Models.Transaction
+import Models.TransactionType
+
 object TransactionManager {
     private val transactions = mutableListOf<Transaction>()
 
@@ -8,10 +10,25 @@ object TransactionManager {
         {
             println("Musí být kladné")
         }
+        if(transaction.type== TransactionType.EXPENSE)
+        {
+            transaction.amount*-1
+        }
         transactions.add(transaction)
         println(transactions)
 
 
     }
     fun getAll(): List<Transaction> = transactions
+    fun sumarize():Int
+    {
+        var sum= 0
+        for(transaction in transactions)
+        {
+            sum += transaction.amount
+
+        }
+        return sum
+
+    }
 }
